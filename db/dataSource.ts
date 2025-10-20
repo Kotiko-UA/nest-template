2a63c7535de2811b9f192270e4ac297c8e4bba57
kotiko <megasuslik666@gmail.com>
2025-10-20 17:18:27 +0300

    fix

--- db/dataSource.ts
+++ db/dataSource.ts
@@ -2,11 +2,8 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import * as Entities from './entities';
 import * as dotenv from 'dotenv';
-import { join } from 'path';
 
 dotenv.config({ path: '.env' });
-
-const isProd = process.env.NODE_ENV === 'production';
 
 export default new DataSource({
   type: 'postgres',
@@ -35,8 +32,6 @@ export default new DataSource({
       ? process.env.DB_NAME
       : undefined,
   entities: Object.values(Entities),
-  migrations: [
-    isProd ? join(__dirname, 'db/migrations/*.ts') : 'db/migrations/*.ts',
-  ],
+  migrations: ['./db/migrations/*.ts'],
   synchronize: false,
 });
