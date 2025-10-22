75b646e6a04980333595701e61fd41cdd991a0aa
kotiko <megasuslik666@gmail.com>
2025-10-22 13:01:43 +0300

    fix - del festy

--- src/main.ts
+++ src/main.ts
@@ -5,35 +5,24 @@ import { GlobalExceptionFilter } from './common/filters/exception.filter';
 import { ResponseInterseptor } from './common/interseptors/response.interseptor';
 import { ValidationPipe } from './common/pipe/pipe';
 import { json } from 'express';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
-import fmp from '@fastify/multipart';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
-  const logger = new Logger();
-  const fastifyAdapter = new FastifyAdapter();
-  fastifyAdapter.register(fmp, {
-    limits: {
-      fieldNameSize: 100, // Max field name size in bytes
-      fieldSize: 10000000, // Max field value size in bytes
-      fields: 20, // Max number of non-file fields
-      files: 2, // Max number of file fields
-      headerPairs: 2000, // Max number of header key=>value pairs
-    },
-  });
+  const logger = new Logger('Bootstrap');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule); // БЕЗ FastifyAdapter
+
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterseptor());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  // app.enableCors({ origin: [/^https:\/\/(www\.)?kotiko\.work$/] });
   app.use(json({ limit: '100mb' }));
-  await app.listen(process.env.API_PORT ?? 3000, () => {
-    logger.log('Api started on port: ' + (process.env.API_PORT ?? 3000));
-  });
+
+  const port = Number(process.env.API_PORT ?? 3000);
+  await app.listen(port);
+  logger.log('Api started on port: ' + port);
 }
 bootstrap();
